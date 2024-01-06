35a105119b801cd1dd86665be6e744e9b38b2c75
Duongsuny <duongsunny03@gmail.com>
2024-01-07 06:10:11 +0700

    some late night changes in insta

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:playground/pages/air_quality_page.dart';
+import 'package:playground/tabs/feed_view.dart';
+import 'package:playground/tabs/gallery_view.dart';
+import 'package:playground/tabs/tagged_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +12,50 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const List<Widget> tabBarView = [FeedView(), GalleryView(), TaggedView()];
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AirQualityPage(),
+      home: DefaultTabController(
+        initialIndex: 0,
+        length: 3,
+        child: Scaffold(
+            appBar: AppBar(),
+            body: SafeArea(
+              child: Column(children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Column(children: [Text("312"), Text("Followers")]),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      decoration: const BoxDecoration(
+                          shape: BoxShape.circle, color: Colors.grey),
+                    ),
+                    const Column(children: [Text("252"), Text("Followings")])
+                  ],
+                ),
+                const SizedBox(height: 10),
+                const Text("Lê Quý Dương | Sciencetist", style: TextStyle(
+                  fontWeight: FontWeight.bold
+                )),
+                const SizedBox(height: 10),
+                const Text("facebook.com/duberlin1211", style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontWeight: FontWeight.bold
+                )),
+                const SizedBox(height: 20),
+                const TabBar(
+                  tabs: [
+                    Tab(icon: Icon(Icons.feed)),
+                    Tab(icon: Icon(Icons.image)),
+                    Tab(icon: Icon(Icons.tag))
+                  ],
+                ),
+                const Expanded(child: TabBarView(children: tabBarView)),
+              ]),
+            )),
+      ),
     );
   }
 }
